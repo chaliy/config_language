71c8f6fc0b0eb2c3ae223aec05cda78f8afc4f9c
Mike Chaliy <mike@chaliy.name>
2009-09-05 00:13:22 +0300

    Initial commit

---
+++ Example/DataConnection.cs
@@ -0,0 +1,92 @@
+﻿namespace Community.Example
+{
+	using System.Configuration;
+	using System.Diagnostics.Contracts;
+
+	public class DataConnectionConfiguration : ConfigurationSection
+	{
+		private const string Path = "community/data";
+
+		private static readonly ConfigurationPropertyCollection PropertyRegistry =
+			new ConfigurationPropertyCollection
+			{
+				new ConfigurationProperty("server", typeof (string), null, ConfigurationPropertyOptions.IsRequired),
+				new ConfigurationProperty("database", typeof (string), null, ConfigurationPropertyOptions.IsRequired),
+				new ConfigurationProperty("username", typeof (string), null, ConfigurationPropertyOptions.IsRequired),
+				new ConfigurationProperty("timeout", typeof (int), null, ConfigurationPropertyOptions.IsRequired),
+			};
+
+		protected override ConfigurationPropertyCollection Properties
+		{
+			get
+			{
+				return PropertyRegistry;
+			}
+		}
+	
+		[ConfigurationProperty("server", IsRequired = true)]
+		public string Server
+		{
+			get
+			{
+				return (string)base["server"];
+			}
+			set
+			{
+				base["server"] = value;
+			}
+		}
+	
+		[ConfigurationProperty("database", IsRequired = true)]
+		public string Database
+		{
+			get
+			{
+				return (string)base["database"];
+			}
+			set
+			{
+				base["database"] = value;
+			}
+		}
+	
+		[ConfigurationProperty("username", IsRequired = true)]
+		public string Username
+		{
+			get
+			{
+				return (string)base["username"];
+			}
+			set
+			{
+				base["username"] = value;
+			}
+		}
+	
+		[ConfigurationProperty("timeout", IsRequired = true)]
+		public int Timeout
+		{
+			get
+			{
+				return (int)base["timeout"];
+			}
+			set
+			{
+				base["timeout"] = value;
+			}
+		}
+
+		public static DataConnectionConfiguration GetDefault()
+		{
+			Contract.Ensures(Contract.Result<DataConnectionConfiguration>() != null);
+
+			var configuration = (DataConnectionConfiguration)ConfigurationManager.GetSection(Path);
+			if (configuration == null)
+			{
+				throw new ConfigurationErrorsException("Configuration section ('community/data') was not found.");
+			}
+			
+			return configuration;
+		}
+	}
+}
---
+++ Example/Programm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Community.Example
+{
+	class Programm
+	{
+		public static void Main()
+		{
+			var config = DataConnectionConfiguration.GetDefault();
+
+			Console.WriteLine("Server: {0}", config.Server);
+			Console.WriteLine("Database: {0}", config.Database);
+			Console.WriteLine("Username: {0}", config.Username);
+			Console.WriteLine("Timeout: {0}", config.Timeout);
+		}
+	}
+}
